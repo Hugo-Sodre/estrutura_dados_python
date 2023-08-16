4f52f170422e69de02c134fb0b0bc366f540d6f1
Hugo Sodré <hugosodre031@gmail.com>
2023-08-16 13:07:01 -0300

    metodos_lista

---
+++ metodos_lista.py
@@ -0,0 +1,26 @@
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40,30,20])
+
+print(lista)
+
+lista.clear()
+
+print(lista)
+
+lista.copy()
+print(lista)
+
+linguagens = ["python", "js", "c", "java", "c#"]
+
+linguagens.index("java")
+linguagens.index("python")
+
+# linguagens.pop()
+# print(linguagens)
+#
+# linguagens.remove("c")
+linguagens.sort()
+print(linguagens)
