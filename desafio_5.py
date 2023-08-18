2c115017b926e5fc45f9e5da07ccefbea9e002b3
Hugo Sodré <hugosodre031@gmail.com>
2023-08-18 18:59:24 -0300

    desafios_5

---
+++ desafio_5.py
@@ -0,0 +1,13 @@
+numPedidos = int(input())
+
+for i in range(1, numPedidos + 1):
+  prato = input()
+  calorias = int(input())
+  ehVegano = input()
+
+  if ehVegano == "s":
+      ehVeganoTexto = "Vegano"
+  else:
+      ehVeganoTexto = "Nao-vegano"
+
+  print(f"Pedido {i}: {prato} ({ehVeganoTexto}) - {calorias} calorias")
