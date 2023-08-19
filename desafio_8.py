e47d5759337bee5dbd932eb170da3a6c18512bce
Hugo Sodré <hugosodre031@gmail.com>
2023-08-19 00:17:15 -0300

    desafio_8

---
+++ desafio_8.py
@@ -0,0 +1,10 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+# Calcula a diferença entre os produtos
+diferenca = (A * B - C * D)
+
+# Imprime a diferença formatada
+print("DIFERENCA =", diferenca)
